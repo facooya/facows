64ed8c8bbb0c43c72f071dfd2c1f8c2e17dec555
Facooya <facooya@gmail.com>
2026-04-19 09:23:49 +0900

    refactor(file_conf): Apply consistency exit and separate from parsing to building

--- src/file_conf.c
+++ src/file_conf.c
@@ -15,81 +15,108 @@
 
 #define CONF_KEY_MAX 16
 
+static int _conf_build(struct fws_conf *conf, const char *conf_buf, size_t conf_len);
 static int _conf_write(const char *conf_buf, char *conf_dst, size_t conf_str_n);
 
-int file_conf_parse(const char *path, struct fws_conf *config) {
-	const char *key[] = {"HTTP_PORT", "HTTPS_PORT", "DOMAIN", "WEB_ROOT", "WEB_LOG", "SSL_CERT", "SSL_KEY"};
+int file_conf_parse(const char *path, struct fws_conf *conf) {
+	int ret = 0;
+	int conf_fd = -1;
+	char *conf_buf = NULL;
 
-	int conf_fd = open(path, O_RDONLY);
+	conf_fd = open(path, O_RDONLY);
 	if (conf_fd < 0) {
-		return -1;
+		ret = -1;
+		goto out;
 	}
 
 	struct stat conf_stat;
 	fstat(conf_fd, &conf_stat);
 
 	size_t conf_len = conf_stat.st_size + 1;
-	char *conf_buf = malloc(conf_len+1);
+	conf_buf = malloc(conf_len+1);
 	if (read(conf_fd, conf_buf, conf_len-1) < 0) {
-		return -1;
+		ret = -1;
+		goto out;
 	}
+	close(conf_fd);
+	conf_fd = -1;
+
 	conf_buf[conf_len-1] = '\n';
 	conf_buf[conf_len] = '\0';
 
-	const char *p1 = conf_buf;
+	if (_conf_build(conf, conf_buf, conf_len)) {
+		ret = -1;
+		goto out;
+	}
+
+	ret = 0;
+out:
+	free(conf_buf);
+	conf_buf = NULL;
+	if (conf_fd >= 0) {
+		close(conf_fd);
+		conf_fd = -1;
+	}
+	return ret;
+}
+
+static int _conf_build(struct fws_conf *conf, const char *conf_buf, size_t conf_len) {
+	const char *key[] = {"HTTP_PORT", "HTTPS_PORT", "DOMAIN", "WEB_ROOT", "WEB_LOG", "SSL_CERT", "SSL_KEY"};
+
+	const char *p = conf_buf;
 	size_t total_n = 0;
 	while (total_n < conf_len) {
-		if (*p1 == '#') {
+		if (*p == '#') {
 			goto next_line;
 		}
 
 		for (size_t i=0; i<sizeof(key)/sizeof(key[0]); i++) {
-			size_t conf_key_len = fac_memclen(p1, ' ', CONF_KEY_MAX);
+			size_t conf_key_len = fac_memclen(p, ' ', CONF_KEY_MAX);
 			if (conf_key_len == CONF_KEY_MAX) {
 				goto next_line;
 			}
 
 			size_t key_len = fac_memclen(key[i], '\0', CONF_KEY_MAX);
 			if (conf_key_len == key_len) {
-				if (memcmp(p1, key[i], key_len) == 0) {
-					p1 += conf_key_len;
+				if (memcmp(p, key[i], key_len) == 0) {
+					p += conf_key_len;
 					total_n += conf_key_len;
-					while (*p1 == ' ') {
+					while (*p == ' ') {
 						total_n++;
-						p1++;
+						p++;
 					}
 
 					switch (i) {
 						case 0:
-							config->http_port = (uint16_t) strtol(p1, NULL, 10);
+							conf->http_port = (uint16_t) strtol(p, NULL, 10);
 							break;
 
 						case 1:
-							config->https_port = (uint16_t) strtol(p1, NULL, 10);
+							conf->https_port = (uint16_t) strtol(p, NULL, 10);
 							break;
 
 						case 2:
-							if (_conf_write(p1, config->domain, sizeof(config->domain)) != 0) {
+							if (_conf_write(p, conf->domain, sizeof(conf->domain)) != 0) {
 								return -1;
 							}
 							break;
 						case 3:
-							if (_conf_write(p1, config->web_root, sizeof(config->web_root)) != 0) {
+							if (_conf_write(p, conf->web_root, sizeof(conf->web_root)) != 0) {
 								return -1;
 							}
 							break;
 						case 4:
-							if (_conf_write(p1, config->web_log, sizeof(config->web_log)) != 0) {
+							if (_conf_write(p, conf->web_log, sizeof(conf->web_log)) != 0) {
 								return -1;
 							}
 							break;
 						case 5:
-							if (_conf_write(p1, config->ssl_cert, sizeof(config->ssl_cert)) != 0) {
+							if (_conf_write(p, conf->ssl_cert, sizeof(conf->ssl_cert)) != 0) {
 								return -1;
 							}
 							break;
 						case 6:
-							if (_conf_write(p1, config->ssl_key, sizeof(config->ssl_key)) != 0) {
+							if (_conf_write(p, conf->ssl_key, sizeof(conf->ssl_key)) != 0) {
 								return -1;
 							}
 							break;
@@ -101,17 +128,15 @@ int file_conf_parse(const char *path, struct fws_conf *config) {
 		}
 
 next_line:
-		size_t n = fac_memclen(p1, '\n', conf_len - total_n);
+		size_t n = fac_memclen(p, '\n', conf_len - total_n);
 		total_n += n + 1;
-		p1 += n + 1;
-		while (*p1 == '\n') {
+		p += n + 1;
+		while (*p == '\n') {
 			total_n++;
-			p1++;
+			p++;
 		}
 	}
 
-	free(conf_buf);
-	close(conf_fd);
 	return 0;
 }
 
